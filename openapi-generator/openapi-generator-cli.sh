95d5028e85286bfb39919694bb0c2e1f17feccd4
jinagada <kimyg3174@gmail.com>
2021-01-18 16:27:08 +0900

    openapi-generator-cli.sh 내용 수정

--- openapi-generator/openapi-generator-cli.sh
+++ openapi-generator/openapi-generator-cli.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 OG_CLI="./openapi-generator-cli.jar"
-${JAVA_HOME}/bin/java -jar ${OG_CLI} "$@"
+JAVA_HOME=`/usr/libexec/java_home -v 1.8 2> /dev/null`
+if [ $JAVA_HOME ] ;then
+        ${JAVA_HOME}/bin/java -jar ${OG_CLI} "$@"
+else
+        /bin/java -jar ${OG_CLI} "$@"
+fi
