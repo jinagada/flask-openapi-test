9834bcb98a993e0a477e58736bb9c1d3e8423844
jinagada <kimyg3174@gmail.com>
2021-01-19 15:54:27 +0900

    POST, PUT, DELETE 추가
    - controller.mustache 파일 수정
    - HttpStatus 객체 Flask 기본 필드 형식으로 변경
    - requestBody 예제 추가
    - POST, PUT, DELETE 메서드 예제 추가

--- src/server_impl/controllers/HttpStatus.py
+++ src/server_impl/controllers/HttpStatus.py
@@ -2,69 +2,112 @@ from openapi_test.models.http_status_message import HttpStatusMessage
 
 
 class HttpStatus:
-    def __init__(self, status_code, message, error_code=None, error_message=None):
+    def __init__(self, title, status, detail, type_txt='about:blank', error_code=None, error_args=None):
         """
         변수 설정
+        :param title:
+        :param status:
+        :param detail:
+        :param type_txt:
         :param error_code:
-        :param error_message:
+        :param error_args:
         """
-        self.status_code = status_code
-        self.message = message
+        self.title = title
+        self.status = status
+        self.detail = detail
+        self.type = type_txt
         self.error_code = error_code
-        self.error_message = error_message
+        self.error_args = error_args
 
     def to_tuple(self):
         """
         Tuple 로 변환하여 반환
         :return:
         """
-        result = HttpStatusMessage(message=self.message, error_code=self.error_code, error_message=self.error_message)
-        return result, self.status_code
+        temp_obj = HttpStatusMessage(title=self.title, status=self.status, detail=self.detail, type_txt=self.type,
+                                     error_code=self.error_code, error_args=self.error_args)
+        temp_dict = temp_obj.to_dict()
+        temp_dict['type'] = temp_dict['type_txt']
+        del temp_dict['type_txt']
+        result = temp_dict
+        return result, self.status
+
+
+class Success(HttpStatus):
+    """
+    Created Retrun
+    """
+    def __init__(self):
+        super().__init__('', 200, '')
+
+    def to_tuple(self, result_dict=None):
+        """
+        Tuple 로 변환하여 반환
+        :return:
+        """
+        result = result_dict.to_dict()
+        return result, self.status
+
+
+class Created(HttpStatus):
+    """
+    Created Retrun
+    """
+    def __init__(self, title):
+        super().__init__(title, 201, title)
 
 
 class NoContent(HttpStatus):
     """
-    Bad Request Retrun
+    No Content Retrun
     """
-    def __init__(self, message):
-        super().__init__(204, message)
+    def __init__(self, title):
+        super().__init__(title, 204, title)
 
 
 class BadRequest(HttpStatus):
     """
     Bad Request Retrun
     """
-    def __init__(self, error_code=None, error_message=None):
-        super().__init__(400, 'Bad Request', error_code, error_message)
+    def __init__(self, detail):
+        super().__init__('Bad Request', 400, detail)
 
 
 class Unauthorized(HttpStatus):
     """
     Unauthorized Retrun
     """
-    def __init__(self, error_code=None, error_message=None):
-        super().__init__(401, 'Unauthorized', error_code, error_message)
+    def __init__(self, error_code=None, detail=None):
+        super().__init__('Unauthorized', 401, detail, '', error_code)
 
 
 class Forbidden(HttpStatus):
     """
     Forbidden Retrun
     """
-    def __init__(self, error_code=None, error_message=None):
-        super().__init__(403, 'Forbidden', error_code, error_message)
+    def __init__(self, error_code=None, detail=None):
+        super().__init__('Forbidden', 403, detail, '', error_code)
 
 
 class NotFound(HttpStatus):
     """
     Not Found Retrun
     """
-    def __init__(self, message='Not Found', error_code=None, error_message=None):
-        super().__init__(404, message, error_code, error_message)
+    def __init__(self, detail='Not Found'):
+        super().__init__('Not Found', 404, detail)
+
+
+class Conflict(HttpStatus):
+    """
+    Conflict Retrun
+    """
+    def __init__(self, error_code=None, detail=None):
+        super().__init__('Conflict', 409, detail, '', error_code)
 
 
 class ServerError(HttpStatus):
     """
     500 Server Error Return
     """
-    def __init__(self, message='Server Error', error_code=None, error_message=None):
-        super().__init__(500, message, error_code, error_message)
+    def __init__(self, detail='Server Error', error_code=None, error_args=None):
+        super().__init__('Server Error', 500, detail, '', error_code, error_args)
--- src/server_impl/controllers/SampleController.py
+++ src/server_impl/controllers/SampleController.py
@@ -13,39 +13,152 @@ common_config = CommonConfig()
 
 def get_index_v1(code):
     """
-    테스트용 API
+    테스트용 API - GET
     :param code:
     :return:
     """
     try:
-        sample = Sample()
-        sample.code = 'SUCC'
-        sample.message = 'Welcome to OpenAPI'
-        logger.info(f'Welcome Url Request : {sample}')
+        logger.info(f'get_index_v1 code : {code}')
+        sample = Sample('SUCC', 'Welcome to OpenAPI')
         logger.info(f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}')
         if code == 204:
             result = NoContent('조회 결과가 없습니다.').to_tuple()
             logger.error(f'get_index_v1 ERROR : {result}')
         elif code == 400:
-            result = BadRequest(error_code='PARAM_ERR', error_message='필수값이 없습니다.').to_tuple()
+            result = BadRequest('필수값이 없습니다.').to_tuple()
             logger.error(f'get_index_v1 ERROR : {result}')
         elif code == 401:
-            result = Unauthorized(error_code='USER_NOT_FOUND', error_message='사용자 정보 없음').to_tuple()
+            result = Unauthorized(error_code='USER_NOT_FOUND', detail='사용자 정보 없음').to_tuple()
             logger.error(f'get_index_v1 ERROR : {result}')
         elif code == 403:
-            result = Forbidden(error_code='ROLE_ERROR', error_message='접근 권한 없음').to_tuple()
+            result = Forbidden(error_code='ROLE_ERROR', detail='접근 권한 없음').to_tuple()
             logger.error(f'get_index_v1 ERROR : {result}')
         elif code == 404:
-            result = NotFound(message='User Info Not Found', error_code='NO_DATA',
-                              error_message='사용자 정보 조회 결과 없음').to_tuple()
+            result = NotFound('사용자 정보 조회 결과 없음').to_tuple()
             logger.error(f'get_index_v1 ERROR : {result}')
         elif code == 500:
             raise Exception('오류발생')
         else:
-            result = sample.to_dict(), 200
+            result = Success().to_tuple(sample)
     except Exception as e:
-        result = ServerError(message='Server Error', error_code='UNKNOWN',
-                             error_message=f'{e}').to_tuple()
+        result = ServerError(detail=f'{e}', error_code='UNKNOWN', error_args=f'{e.args}').to_tuple()
         logger.error(f'get_index_v1 ERROR : {result}')
         err_log(logger, e, 'get_index_v1')
+    return result
+
+
+def post_index_v1(code, sample_body):
+    """
+    테스트용 API - POST
+    :param code:
+    :param sample_body:
+    :return:
+    """
+    try:
+        logger.info(f'post_index_v1 code, sample_body : {code}, {sample_body}')
+        sample = Sample(sample_body.code, sample_body.message)
+        logger.info(f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}')
+        if code == 204:
+            result = NoContent('조회 결과가 없습니다.').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 400:
+            result = BadRequest('필수값이 없습니다.').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 401:
+            result = Unauthorized(error_code='USER_NOT_FOUND', detail='사용자 정보 없음').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 403:
+            result = Forbidden(error_code='ROLE_ERROR', detail='접근 권한 없음').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 404:
+            result = NotFound('사용자 정보 조회 결과 없음').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 409:
+            result = Conflict(error_code='DUPLICATE', detail='자료 중복').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 500:
+            raise Exception('오류발생')
+        else:
+            result = Success().to_tuple(sample)
+    except Exception as e:
+        result = ServerError(detail=f'{e}', error_code='UNKNOWN', error_args=f'{e.args}').to_tuple()
+        logger.error(f'post_index_v1 ERROR : {result}')
+        err_log(logger, e, 'post_index_v1')
+    return result
+
+
+def put_index_v1(code, sample_body):
+    """
+    테스트용 API - PUT
+    :param code:
+    :param sample_body:
+    :return:
+    """
+    try:
+        logger.info(f'put_index_v1 code, sample_body : {code}, {sample_body}')
+        sample = Sample(sample_body.code, sample_body.message)
+        logger.info(f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}')
+        if code == 201:
+            result = Created('Succss').to_tuple()
+            logger.info(f'put_index_v1 Created : {result}')
+        elif code == 400:
+            result = BadRequest('필수값이 없습니다.').to_tuple()
+            logger.error(f'put_index_v1 ERROR : {result}')
+        elif code == 401:
+            result = Unauthorized(error_code='USER_NOT_FOUND', detail='사용자 정보 없음').to_tuple()
+            logger.error(f'put_index_v1 ERROR : {result}')
+        elif code == 403:
+            result = Forbidden(error_code='ROLE_ERROR', detail='접근 권한 없음').to_tuple()
+            logger.error(f'put_index_v1 ERROR : {result}')
+        elif code == 409:
+            result = Forbidden(error_code='DUPLICATE', detail='자료 중복').to_tuple()
+            logger.error(f'put_index_v1 ERROR : {result}')
+        elif code == 500:
+            raise Exception('오류발생')
+        else:
+            result = Success().to_tuple(sample)
+    except Exception as e:
+        result = ServerError(detail=f'{e}', error_code='UNKNOWN', error_args=f'{e.args}').to_tuple()
+        logger.error(f'put_index_v1 ERROR : {result}')
+        err_log(logger, e, 'put_index_v1')
+    return result
+
+
+def delete_index_v1(code, sample_body=None):
+    """
+    테스트용 API - DELETE
+    :param code:
+    :param sample_body:
+    :return:
+    """
+    try:
+        logger.info(f'post_index_v1 code, sample_body : {code}, {sample_body}')
+        sample = Sample(sample_body.code, sample_body.message)
+        logger.info(f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}')
+        if code == 204:
+            result = NoContent('데이터가 존재하지 않습니다.(삭제 완료 포함)').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 400:
+            result = BadRequest('필수값이 없습니다.').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 401:
+            result = Unauthorized(error_code='USER_NOT_FOUND', detail='사용자 정보 없음').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 403:
+            result = Forbidden(error_code='ROLE_ERROR', detail='접근 권한 없음').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 404:
+            result = NotFound('삭제할 데이터가 없습니다.').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 409:
+            result = Conflict(error_code='CONFLICT', detail='삭제할 수 없는 상태 입니다.').to_tuple()
+            logger.error(f'post_index_v1 ERROR : {result}')
+        elif code == 500:
+            raise Exception('오류발생')
+        else:
+            result = Success().to_tuple(sample)
+    except Exception as e:
+        result = ServerError(detail=f'{e}', error_code='UNKNOWN', error_args=f'{e.args}').to_tuple()
+        logger.error(f'post_index_v1 ERROR : {result}')
+        err_log(logger, e, 'post_index_v1')
     return result
