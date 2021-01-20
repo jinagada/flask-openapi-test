b6529dbc7b98d4d0a9dd4e44e1ef722526a58dcf
jinagada <kimyg3174@gmail.com>
2021-01-20 16:32:19 +0900

    debug_log 추가

--- src/server_impl/controllers/SampleController.py
+++ src/server_impl/controllers/SampleController.py
@@ -5,7 +5,7 @@ from flask import current_app
 from openapi_test.models.sample import Sample
 from .HttpStatus import *
 from ..config.config import CommonConfig
-from ..service.LogService import err_log
+from ..service.LogService import *
 
 logger = logging.getLogger('flask-openapi-test.SampleController')
 common_config = CommonConfig()
@@ -18,31 +18,31 @@ def get_index_v1(code):
     :return:
     """
     try:
-        logger.info(f'get_index_v1 code : {code}')
+        debug_log(logger, f'code : {code}', 'get_index_v1')
         sample = Sample('SUCC', 'Welcome to OpenAPI')
-        logger.info(f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}')
+        debug_log(logger, f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}', 'get_index_v1')
         if code == 204:
             result = NoContent('조회 결과가 없습니다.').to_tuple()
-            logger.error(f'get_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'get_index_v1')
         elif code == 400:
             result = BadRequest('필수값이 없습니다.').to_tuple()
-            logger.error(f'get_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'get_index_v1')
         elif code == 401:
             result = Unauthorized(error_code='USER_NOT_FOUND', detail='사용자 정보 없음').to_tuple()
-            logger.error(f'get_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'get_index_v1')
         elif code == 403:
             result = Forbidden(error_code='ROLE_ERROR', detail='접근 권한 없음').to_tuple()
-            logger.error(f'get_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'get_index_v1')
         elif code == 404:
             result = NotFound('사용자 정보 조회 결과 없음').to_tuple()
-            logger.error(f'get_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'get_index_v1')
         elif code == 500:
             raise Exception('오류발생')
         else:
             result = Success().to_tuple(sample)
     except Exception as e:
         result = ServerError(detail=f'{e}', error_code='UNKNOWN', error_args=f'{e.args}').to_tuple()
-        logger.error(f'get_index_v1 ERROR : {result}')
+        debug_log(logger, f'ERROR : {result}', 'get_index_v1')
         err_log(logger, e, 'get_index_v1')
     return result
 
@@ -55,34 +55,34 @@ def post_index_v1(code, sample_body):
     :return:
     """
     try:
-        logger.info(f'post_index_v1 code, sample_body : {code}, {sample_body}')
+        debug_log(logger, f'code, sample_body : {code}, {sample_body}', 'post_index_v1')
         sample = Sample(sample_body.code, sample_body.message)
-        logger.info(f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}')
+        debug_log(logger, f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}', 'post_index_v1')
         if code == 204:
             result = NoContent('조회 결과가 없습니다.').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'post_index_v1')
         elif code == 400:
             result = BadRequest('필수값이 없습니다.').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'post_index_v1')
         elif code == 401:
             result = Unauthorized(error_code='USER_NOT_FOUND', detail='사용자 정보 없음').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'post_index_v1')
         elif code == 403:
             result = Forbidden(error_code='ROLE_ERROR', detail='접근 권한 없음').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'post_index_v1')
         elif code == 404:
             result = NotFound('사용자 정보 조회 결과 없음').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'post_index_v1')
         elif code == 409:
             result = Conflict(error_code='DUPLICATE', detail='자료 중복').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'post_index_v1')
         elif code == 500:
             raise Exception('오류발생')
         else:
             result = Success().to_tuple(sample)
     except Exception as e:
         result = ServerError(detail=f'{e}', error_code='UNKNOWN', error_args=f'{e.args}').to_tuple()
-        logger.error(f'post_index_v1 ERROR : {result}')
+        debug_log(logger, f'ERROR : {result}', 'post_index_v1')
         err_log(logger, e, 'post_index_v1')
     return result
 
@@ -95,31 +95,31 @@ def put_index_v1(code, sample_body):
     :return:
     """
     try:
-        logger.info(f'put_index_v1 code, sample_body : {code}, {sample_body}')
+        debug_log(logger, f'code, sample_body : {code}, {sample_body}', 'put_index_v1')
         sample = Sample(sample_body.code, sample_body.message)
-        logger.info(f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}')
+        debug_log(logger, f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}', 'put_index_v1')
         if code == 201:
             result = Created('Succss').to_tuple()
-            logger.info(f'put_index_v1 Created : {result}')
+            debug_log(logger, f'Created : {result}', 'put_index_v1')
         elif code == 400:
             result = BadRequest('필수값이 없습니다.').to_tuple()
-            logger.error(f'put_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'put_index_v1')
         elif code == 401:
             result = Unauthorized(error_code='USER_NOT_FOUND', detail='사용자 정보 없음').to_tuple()
-            logger.error(f'put_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'put_index_v1')
         elif code == 403:
             result = Forbidden(error_code='ROLE_ERROR', detail='접근 권한 없음').to_tuple()
-            logger.error(f'put_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'put_index_v1')
         elif code == 409:
             result = Forbidden(error_code='DUPLICATE', detail='자료 중복').to_tuple()
-            logger.error(f'put_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'put_index_v1')
         elif code == 500:
             raise Exception('오류발생')
         else:
             result = Success().to_tuple(sample)
     except Exception as e:
         result = ServerError(detail=f'{e}', error_code='UNKNOWN', error_args=f'{e.args}').to_tuple()
-        logger.error(f'put_index_v1 ERROR : {result}')
+        debug_log(logger, f'ERROR : {result}', 'put_index_v1')
         err_log(logger, e, 'put_index_v1')
     return result
 
@@ -132,33 +132,33 @@ def delete_index_v1(code, sample_body=None):
     :return:
     """
     try:
-        logger.info(f'post_index_v1 code, sample_body : {code}, {sample_body}')
+        debug_log(logger, f'code, sample_body : {code}, {sample_body}', 'delete_index_v1')
         sample = Sample(sample_body.code, sample_body.message)
-        logger.info(f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}')
+        debug_log(logger, f'config : {common_config.get_config(current_app.config.get("APP_ENV"))}', 'delete_index_v1')
         if code == 204:
             result = NoContent('데이터가 존재하지 않습니다.(삭제 완료 포함)').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'delete_index_v1')
         elif code == 400:
             result = BadRequest('필수값이 없습니다.').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'delete_index_v1')
         elif code == 401:
             result = Unauthorized(error_code='USER_NOT_FOUND', detail='사용자 정보 없음').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'delete_index_v1')
         elif code == 403:
             result = Forbidden(error_code='ROLE_ERROR', detail='접근 권한 없음').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'delete_index_v1')
         elif code == 404:
             result = NotFound('삭제할 데이터가 없습니다.').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'delete_index_v1')
         elif code == 409:
             result = Conflict(error_code='CONFLICT', detail='삭제할 수 없는 상태 입니다.').to_tuple()
-            logger.error(f'post_index_v1 ERROR : {result}')
+            debug_log(logger, f'ERROR : {result}', 'delete_index_v1')
         elif code == 500:
             raise Exception('오류발생')
         else:
             result = Success().to_tuple(sample)
     except Exception as e:
         result = ServerError(detail=f'{e}', error_code='UNKNOWN', error_args=f'{e.args}').to_tuple()
-        logger.error(f'post_index_v1 ERROR : {result}')
-        err_log(logger, e, 'post_index_v1')
+        debug_log(logger, f'ERROR : {result}', 'delete_index_v1')
+        err_log(logger, e, 'delete_index_v1')
     return result
--- src/server_impl/service/LogService.py
+++ src/server_impl/service/LogService.py
@@ -7,3 +7,13 @@ def err_log(logger, e, fn_name):
     """
     logger.error(f'{fn_name} Exception : {e}')
     logger.error(f'{fn_name} Exception args : {e.args}')
+
+
+def debug_log(logger, msg, fn_name):
+    """
+    디버그로그 출력
+    :param logger:
+    :param msg:
+    :param fn_name:
+    """
+    logger.debug(f'{fn_name} {msg}')
